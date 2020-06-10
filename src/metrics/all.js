3844fae0470ba7b8debf04bd50a084541f12eabf
y8 <y8@ya.ru>
2020-06-10 19:34:36 +0300

    Move exporter own metrics to `exporter` folder

--- src/metrics/all.js
+++ src/metrics/all.js
@@ -1,15 +1,9 @@
 import Registry from 'src/registry.js'
 
+require('src/metrics/exporter.js')
 require('src/metrics/db.js')
 require('src/metrics/wt.js')
 require('src/metrics/collection.js')
-
-Registry.createMetric(require('src/metrics/probe_runtime_seconds.json'))
-Registry.createMetric(require('src/metrics/probe_runs_total.json'))
-
-Registry.createMetric(require('src/metrics/bridge_runtime_seconds.json'))
-Registry.createMetric(require('src/metrics/bridge_runs_total.json'))
-Registry.createMetric(require('src/metrics/bridge_keys_seen_total.json'))
 
 Registry.createMetric(require('src/metrics/server/server_info.json'))
 Registry.createMetric(require('src/metrics/server/net/server_net_recieved_bytes_total.json'))
---
+++ src/metrics/exporter.js
@@ -0,0 +1,11 @@
+import Registry from 'src/registry.js'
+
+Registry.createMetric(require('src/metrics/exporter/probe_runtime_seconds.json'))
+Registry.createMetric(require('src/metrics/exporter/probe_runs_total.json'))
+
+Registry.createMetric(require('src/metrics/exporter/bridge_runtime_seconds.json'))
+Registry.createMetric(require('src/metrics/exporter/bridge_runs_total.json'))
+Registry.createMetric(require('src/metrics/exporter/bridge_keys_seen_total.json'))
+Registry.createMetric(require('src/metrics/exporter/bridge_keys_exported_total.json'))
+
+Registry.createMetric(require('src/metrics/exporter/registry_compile_time_seconds.json'))
