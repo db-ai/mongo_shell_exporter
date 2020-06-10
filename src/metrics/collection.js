8dca3de1e594cbea659f108e4050d3f5c1ce01d9
y8 <y8@ya.ru>
2020-06-10 23:07:41 +0300

    Metrics: add `collection_scale_factor`

--- src/metrics/collection.js
+++ src/metrics/collection.js
@@ -1,10 +1,29 @@
 import Registry from 'src/registry.js'
 
-Registry.createMetric(require('src/metrics/collection/collection_document_size_avg_bytes.json'))
-Registry.createMetric(require('src/metrics/collection/collection_documents_count.json'))
-Registry.createMetric(require('src/metrics/collection/collection_size_bytes.json'))
-Registry.createMetric(require('src/metrics/collection/collection_storage_size_bytes.json'))
-Registry.createMetric(require('src/metrics/collection/collection_index_count.json'))
-Registry.createMetric(require('src/metrics/collection/collection_index_size_bytes.json'))
-Registry.createMetric(require('src/metrics/collection/collection_capped_max_count.json'))
-Registry.createMetric(require('src/metrics/collection/collection_capped_max_size_bytes.json'))
+Registry.createMetric(
+  require('src/metrics/collection/collection_capped_max_count.json')
+)
+Registry.createMetric(
+  require('src/metrics/collection/collection_capped_max_size_bytes.json')
+)
+Registry.createMetric(
+  require('src/metrics/collection/collection_documents_count.json')
+)
+Registry.createMetric(
+  require('src/metrics/collection/collection_document_size_avg_bytes.json')
+)
+Registry.createMetric(
+  require('src/metrics/collection/collection_index_count.json')
+)
+Registry.createMetric(
+  require('src/metrics/collection/collection_index_size_bytes.json')
+)
+Registry.createMetric(
+  require('src/metrics/collection/collection_scale_factor.json')
+)
+Registry.createMetric(
+  require('src/metrics/collection/collection_size_bytes.json')
+)
+Registry.createMetric(
+  require('src/metrics/collection/collection_storage_size_bytes.json')
+)
