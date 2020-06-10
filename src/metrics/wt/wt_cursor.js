537e031f5047b38922106ce1019041d91dc2837e
y8 <y8@ya.ru>
2020-06-10 22:20:44 +0300

    Add `wt_cursor_calls` metric

--- src/metrics/wt/wt_cursor.js
+++ src/metrics/wt/wt_cursor.js
@@ -1,5 +1,6 @@
 import Registry from 'src/registry.js'
 
+Registry.createMetric(require('src/metrics/wt/cursor/wt_cursor_calls.json'))
 Registry.createMetric(require('src/metrics/wt/cursor/wt_cursor_ops_restarted.json'))
 Registry.createMetric(require('src/metrics/wt/cursor/wt_cursor_inserted_key_value_bytes.json'))
 Registry.createMetric(require('src/metrics/wt/cursor/wt_cursor_removed_key_bytes.json'))
