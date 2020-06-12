2f27c91b92da682d62ceb9dac239035e08168005
y8 <y8@ya.ru>
2020-06-12 18:05:48 +0300

    Metric: add `server_net_secure_connections` metric

--- src/metrics/server.js
+++ src/metrics/server.js
@@ -12,10 +12,13 @@ Registry.createMetric(
   require('src/metrics/server/net/server_net_connections_created.json')
 )
 Registry.createMetric(
+  require('src/metrics/server/net/server_net_recieved_bytes_total.json')
+)
+Registry.createMetric(
   require('src/metrics/server/net/server_net_requests_total.json')
 )
 Registry.createMetric(
-  require('src/metrics/server/net/server_net_recieved_bytes_total.json')
+  require('src/metrics/server/net/server_net_secure_connections.json')
 )
 Registry.createMetric(
   require('src/metrics/server/net/server_net_sent_bytes_total.json')
