08a987b9ef001fbbd3ec70448204d5488e9c38a6
y8 <y8@ya.ru>
2020-06-12 16:45:48 +0300

    Metric: add `server_uptime`

--- src/metrics/server.js
+++ src/metrics/server.js
@@ -1,6 +1,8 @@
 import Registry from 'src/registry.js'
 
 Registry.createMetric(require('src/metrics/server/server_info.json'))
+Registry.createMetric(require('src/metrics/server/server_uptime.json'))
+
 Registry.createMetric(
   require('src/metrics/server/net/server_net_requests_total.json')
 )
