881a060118d663e5b4df5088eb8b9b1487d35ff3
y8 <y8@ya.ru>
2020-06-12 16:44:20 +0300

    Metric: add `server_net_requests_total`

--- src/metrics/all.js
+++ src/metrics/all.js
@@ -1,10 +1,5 @@
-import Registry from 'src/registry.js'
-
-require('src/metrics/exporter.js')
-require('src/metrics/db.js')
-require('src/metrics/wt.js')
 require('src/metrics/collection.js')
-
-Registry.createMetric(require('src/metrics/server/server_info.json'))
-Registry.createMetric(require('src/metrics/server/net/server_net_recieved_bytes_total.json'))
-Registry.createMetric(require('src/metrics/server/net/server_net_sent_bytes_total.json'))
+require('src/metrics/db.js')
+require('src/metrics/exporter.js')
+require('src/metrics/server.js')
+require('src/metrics/wt.js')
---
+++ src/metrics/server.js
@@ -0,0 +1,12 @@
+import Registry from 'src/registry.js'
+
+Registry.createMetric(require('src/metrics/server/server_info.json'))
+Registry.createMetric(
+  require('src/metrics/server/net/server_net_requests_total.json')
+)
+Registry.createMetric(
+  require('src/metrics/server/net/server_net_recieved_bytes_total.json')
+)
+Registry.createMetric(
+  require('src/metrics/server/net/server_net_sent_bytes_total.json')
+)
